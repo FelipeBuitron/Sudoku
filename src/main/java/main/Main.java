e9edcf38544682057a275be1f89a724bdb0e9913
FelipeBuitron <felipe.escobar0821@gmail.com>
2026-05-26 18:20:27 -0500

    Primer commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                getClass().getResource("/view/StartView.fxml")
+        );
+
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+        stage.setTitle("Sudoku");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
